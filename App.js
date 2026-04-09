c8738a19682f3dc167dee043be8bfd1a3588681c
Izadora2ds2025 <steclan.izadora@escola.pr.gov.br>
2026-04-09 09:25:19 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,12 +1,9 @@
-//import Aula01 from "./src/aula01";
-//import Aula02 from "./src/aula02";
-import Aula03 from "./src/aula03";
-
+import Aula01 from "./src/aula01";
+import Aula02 from "./src/aula02";
 export default function App() {
   return (
- //<Aula01/>
- //<Aula02/>
- <Aula03/>
+    //<Aula01 />
+  <Aula02 />
   );
 }
 
